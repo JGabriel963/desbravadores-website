988302fa30207242c26adabf738310fddd5edbbe
João Gabriel <joaogabriel9633@gmail.com>
2023-11-18 11:14:12 -0300

    fix: update project

--- firebase.js
+++ firebase.js
@@ -1,20 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from 'firebase/auth'
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCnrhVUQMdSedQ00XTq3pZ_1ymOwbHQt1I",
-  authDomain: "nova-jerusalem-fdb76.firebaseapp.com",
-  projectId: "nova-jerusalem-fdb76",
-  storageBucket: "nova-jerusalem-fdb76.appspot.com",
-  messagingSenderId: "322829560485",
-  appId: "1:322829560485:web:0979260ee044326f917b4c",
+  apiKey: "AIzaSyA86daypD92fJkBnzJ84_wxl6DlN2W0X_s",
+  authDomain: "clube-nj.firebaseapp.com",
+  projectId: "clube-nj",
+  storageBucket: "clube-nj.appspot.com",
+  messagingSenderId: "232122981403",
+  appId: "1:232122981403:web:ac9980eb177d166c166cbe",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app)
+const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { db, auth };
